b42ca96f8873714d0a815aa71a83a61c0b241b3a
Selin Zeydan <116116043+selinzeydan@users.noreply.github.com>
2022-12-24 22:17:11 +0300

    Update Odev 2.sql

--- Odev 2.sql
+++ Odev 2.sql
@@ -4,7 +4,7 @@
 3. film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
 
 
-SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99 ;
+SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
 SELECT first_name,last_name FROM actor WHERE first_name IN('Penelope','Nick','Ed');
 
